169ed8c05c3d81fb9665776bb79a40162c623723
aravindhan-cpu <85392454+aravindhan-cpu@users.noreply.github.com>
2021-06-27 22:25:43 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Info from "./Note.jsx";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+
+export default App;
